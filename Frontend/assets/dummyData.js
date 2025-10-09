5f2ce047b033e757632c5e541ce8f3ff4f7127d8
imaaryan <imaaryanpandey@gmail.com>
2025-10-09 10:41:29 +0530

    propertyForRent Dummy Json is Added

--- Frontend/assets/dummyData.js
+++ Frontend/assets/dummyData.js
@@ -67,6 +67,58 @@ const propertyToBuy = [
   },
 ];
 
+const propertyForRent = [
+  {
+    _id: "6718da0b1e3f8a5a2e1b4c90",
+    title: "A detailed step by step guide to manage your lifestyle",
+    shortDescription:
+      "<h1>A Simple Step-by-Step Guide to Managing Your Lifestyle</h1><p>If you're looking to improve your health, boost productivity, and create a balanced life, managing your lifestyle intentionally is key. Here's a short guide to help you take control of your daily habits and overall well-being.</p><h2>1. Assess Your Current Lifestyle</h2><p>Track your habits for a week. Note your energy levels, sleep, diet, and daily routines. Reflect on what's working and what needs change.</p>",
+    images: {
+      featuredImage: property1img2,
+      imageGallery: [
+        property1img3,
+        property1img4,
+        property1img5,
+        property1img1,
+      ],
+    },
+    areaId: "6718da0b1e3f8a5a2e1b4c93",
+    amenityIds: ["6718da0b1e3f8a5a2e1b4ca1", "6718da0b1e3f8a5a2e1b4ca2"],
+    propertyType: "Residential Apartment",
+    bedrooms: 3,
+    bathrooms: 2,
+    propertySize: 2000,
+    pricing: {
+      rentPerMonth: 23000,
+      securityDeposit: 50000,
+    },
+    ownerDetails: {
+      currentOwner: "Mr. Yashpal Singh & Mr. Divye Malik resident",
+      currentOwnerPhoneNumber: 9898908989,
+    },
+    propertyDetails: {
+      furnishing: "Semi Furnished",
+      avalabeFor: "Family, Bachelors (Womens Only)",
+      availability: "Immediate",
+      whyConsider:
+        "North-East Facing DPS Indirapuram Nearby Electronic City Noida Nearby FORTIS Nearby",
+      features:
+        "Security / Fire Alarm, Intercom Facility, Lift(s), Water Purifier",
+      society: "Assotech Windsor Park Total 11 Acres (44,452 sq.m)",
+      address:
+        "Shiv Shakti Enclave Rajeshwar Nagar Phase1 Extension Dehradun Dun",
+    },
+    locationOnMap: {
+      latitude: 30.339395816460264,
+      longitude: 78.01859020985364,
+    },
+    createdAt: "2025-04-21T07:06:37.508Z",
+    updatedAt: "2025-04-24T08:26:29.750Z",
+    __v: 0,
+    isPublished: true,
+  },
+];
+
 const city = [
   { _id: "6718da0b1e3f8a5a2e1b4c91", name: "Dehradun" },
   { _id: "6718da0b1e3f8a5a2e1b4c92", name: "Rishikesh" },
