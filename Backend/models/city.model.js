cf047c0b177ffc07fe45cf2af23c9783ce2b4f56
imaaryan <imaaryanpandey@gmail.com>
2025-10-09 16:49:58 +0530

    Model for City Added

---
+++ Backend/models/city.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const citySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+  },
+  { timestamps: true }
+);
+
+const City = mongoose.model("City", citySchema);
+
+export default City;
