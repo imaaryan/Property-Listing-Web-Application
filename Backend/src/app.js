29a888c375d00eb6c15044c12492a80d0fc8d81a
imaaryan <imaaryanpandey@gmail.com>
2025-10-10 10:29:11 +0530

    Enhance CORS configuration and set request body limits in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,8 +3,17 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+// CORS Configration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Authorization"]
+}));
+
 
 app.get("/", (req, res) => {
   res.send("Server is Running");
