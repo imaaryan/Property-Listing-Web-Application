dd6e8cb3b7e7e1606d0c0594dee21ae80995730b
imaaryan <imaaryanpandey@gmail.com>
2025-10-10 10:33:27 +0530

    Update CORS configuration to include PATCH method and Content-Type header

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(cors({
     origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Authorization"]
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Authorization", "Content-Type"]
 }));
 
 
