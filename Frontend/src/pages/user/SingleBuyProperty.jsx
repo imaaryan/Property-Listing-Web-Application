d938da7c78ee036d0a5737899c4fd6326255bec8
imaaryan <imaaryanpandey@gmail.com>
2025-11-25 16:54:21 +0530

    feat: display property short description and enhance single property page layout.

--- Frontend/src/pages/user/SingleBuyProperty.jsx
+++ Frontend/src/pages/user/SingleBuyProperty.jsx
@@ -57,112 +57,130 @@ const SingleBuyProperty = () => {
               alt={currentProperty.title + " Image"}
             />
           </div>
+          <div className="flex flex-col my-6 gap-3 p-6 border-3 border-secondary rounded-xl bg-[#fdfdfd]">
+            <h2 className="text-lg font-medium ">Short Description</h2>
+            <p className="text-base text-gray-500">
+              {currentProperty.shortDescription}
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col w-[720px] gap-3 p-6 border-2 border-secondary rounded-xl bg-[#fdfdfd] ">
-          <div className="badge badge-sm badge-primary rounded-full">
-            {currentProperty.propertyType}
-          </div>
-          <h2 className="text-lg md:text-3xl font-medium">
-            {currentProperty.title}
-          </h2>
-          <p className="flex items-center gap-2 text-gray-500">
-            <RiMapPin2Fill size={15} /> {areaName}, {cityName}
-          </p>
 
-          <h3 className="text-4xl font-medium text-primary">
-            ₹{formatPrice(currentProperty.pricing.finelPricing)}
-          </h3>
+        {/* Sticky Sidebar */}
+        <div className="w-[755px]">
+          <div className="flex flex-col gap-3 p-6 border-3 border-secondary rounded-xl bg-[#fdfdfd] ">
+            {/* Property Type Badge */}
+            <div className="badge badge-sm badge-primary rounded-full">
+              {currentProperty.propertyType}
+            </div>
 
-          <div className="flex flex-row justify-start gap-2 my-3 ">
-            <div
-              className={
-                !currentProperty.bedrooms
-                  ? "hidden"
-                  : "flex flex-row items-center gap-2 p-3 bg-[#1244e320] hover:bg-[#1244e330] select-none text-sm rounded-lg"
+            {/* Property Title */}
+            <h2 className="text-lg md:text-3xl font-medium">
+              {currentProperty.title}
+            </h2>
+
+            {/* Property Location */}
+            <p className="flex items-center gap-2 text-gray-500">
+              <RiMapPin2Fill size={15} /> {areaName}, {cityName}
+            </p>
+
+            {/* Property Price */}
+            <h3 className="text-4xl font-medium text-primary">
+              ₹{formatPrice(currentProperty.pricing.finelPricing)}
+            </h3>
+
+            {/* Property BHK & Size Info */}
+            <div className="flex flex-row justify-start gap-2 my-3 ">
+              <div
+                className={
+                  !currentProperty.bedrooms
+                    ? "hidden"
+                    : "flex flex-row items-center gap-2 p-3 bg-[#1244e320] hover:bg-[#1244e330] select-none text-sm rounded-lg"
+                }
+              >
+                <RiHotelBedFill size={24} color="#1244e3" />{" "}
+                {currentProperty.bedrooms + " Bedrooms"}
+              </div>
+
+              <div
+                className={
+                  !currentProperty.bathrooms
+                    ? "hidden"
+                    : "flex flex-row items-center gap-2 p-3 bg-[#1244e320] hover:bg-[#1244e330] select-none text-sm rounded-lg"
+                }
+              >
+                <RiHandSanitizerFill size={23} color="#1244e3" />{" "}
+                {currentProperty.bathrooms + " Bathrooms"}
+              </div>
+              {(() => {
+                const { size, unit } = convertToYardsIfNeeded(
+                  currentProperty.propertySize,
+                  currentProperty.propertyType
+                );
+                return (
+                  <div className="flex flex-row items-center gap-2 p-3 bg-[#1244e320] hover:bg-[#1244e330] select-none text-sm rounded-lg">
+                    <RiShadowFill size={22} color="#1244e3" /> {size} {unit}
+                  </div>
+                );
+              })()}
+            </div>
+
+            {/* Khatauni Details */}
+            <div className="flex flex-col gap-3 mb-3">
+              <h2 className="text-xl font-medium">Khatauni Details</h2>
+              <div>
+                <h3 className="text-md font-medium ">Current Owner:</h3>
+                <p className="text-base text-gray-500">
+                  {currentProperty.khatuniDetails.currentOwner}
+                </p>
+              </div>
+              <div>
+                <h3 className="text-md font-medium ">Previous Owner:</h3>
+                <p className="text-base text-gray-500">
+                  {currentProperty.khatuniDetails.previousOwner}
+                </p>
+              </div>
+              <div c>
+                <h3 className="text-md font-medium ">Khasra Number:</h3>
+                <p className="text-base text-gray-500">
+                  {currentProperty.khatuniDetails.khasraNumber}
+                </p>
+              </div>
+            </div>
+
+            <button
+              className="btn btn-primary text-lg font-normal "
+              onClick={() =>
+                (window.location.href = `tel:${currentProperty.khatuniDetails.currentOwnerPhoneNumber}`)
               }
             >
-              <RiHotelBedFill size={24} color="#1244e3" />{" "}
-              {currentProperty.bedrooms + " Bedrooms"}
-            </div>
+              <RiPhoneLine size={22} />
+              Call Current Owner
+            </button>
 
-            <div
-              className={
-                !currentProperty.bathrooms
-                  ? "hidden"
-                  : "flex flex-row items-center gap-2 p-3 bg-[#1244e320] hover:bg-[#1244e330] select-none text-sm rounded-lg"
+            <button
+              className="btn btn-outline text-lg font-normal "
+              onClick={() =>
+                navigate(
+                  `/property/${propertyFor === "Buy" ? "sale" : "rent"}/${_id}`
+                )
               }
             >
-              <RiHandSanitizerFill size={23} color="#1244e3" />{" "}
-              {currentProperty.bathrooms + " Bathrooms"}
-            </div>
-            {(() => {
-              const { size, unit } = convertToYardsIfNeeded(
-                currentProperty.propertySize,
-                currentProperty.propertyType
-              );
-              return (
-                <div className="flex flex-row items-center gap-2 p-3 bg-[#1244e320] hover:bg-[#1244e330] select-none text-sm rounded-lg">
-                  <RiShadowFill size={22} color="#1244e3" /> {size} {unit}
-                </div>
-              );
-            })()}
+              <RiInbox2Line size={22} />
+              Send Enquiry
+            </button>
+
+            <button
+              className="btn btn-outline text-lg font-normal "
+              onClick={() =>
+                navigate(
+                  `/property/${propertyFor === "Buy" ? "sale" : "rent"}/${_id}`
+                )
+              }
+            >
+              <RiCalculatorLine size={22} />
+              EMI Calculator
+            </button>
           </div>
-
-          <div className="flex flex-col gap-3 mb-3">
-            <h2 className="text-xl font-medium">Khatauni Details</h2>
-            <div>
-              <h3 className="text-md font-medium ">Current Owner:</h3>
-              <p className="text-base text-gray-500">
-                {currentProperty.khatuniDetails.currentOwner}
-              </p>
-            </div>
-            <div>
-              <h3 className="text-md font-medium ">Previous Owner:</h3>
-              <p className="text-base text-gray-500">
-                {currentProperty.khatuniDetails.previousOwner}
-              </p>
-            </div>
-            <div c>
-              <h3 className="text-md font-medium ">Khasra Number:</h3>
-              <p className="text-base text-gray-500">
-                {currentProperty.khatuniDetails.khasraNumber}
-              </p>
-            </div>
-          </div>
-
-          <button
-            className="btn btn-primary text-lg font-normal "
-            onClick={() =>
-              (window.location.href = `tel:${currentProperty.khatuniDetails.currentOwnerPhoneNumber}`)
-            }
-          >
-            <RiPhoneLine size={22} />
-            Call Current Owner
-          </button>
-
-          <button
-            className="btn btn-outline text-lg font-normal "
-            onClick={() =>
-              navigate(
-                `/property/${propertyFor === "Buy" ? "sale" : "rent"}/${_id}`
-              )
-            }
-          >
-            <RiInbox2Line size={22} />
-            Send Enquiry
-          </button>
-
-          <button
-            className="btn btn-outline text-lg font-normal "
-            onClick={() =>
-              navigate(
-                `/property/${propertyFor === "Buy" ? "sale" : "rent"}/${_id}`
-              )
-            }
-          >
-            <RiCalculatorLine size={22} />
-            EMI Calculator
-          </button>
         </div>
       </div>
     </>
