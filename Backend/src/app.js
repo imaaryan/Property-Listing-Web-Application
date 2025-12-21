452b22f3587ab91bf6de2a8f1cfa6ff4860598f1
imaaryan <imaaryanpandey@gmail.com>
2025-12-21 23:40:17 +0530

    feat: support multiple CORS origins from environment variable

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,7 +16,9 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 // CORS Configration
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN
+      ? process.env.CORS_ORIGIN.split(",")
+      : "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Authorization", "Content-Type"],
